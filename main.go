b9041a97821d3ca4168edf66e6affb91b0dea0f0
Urmzd Mukhammadnaim <urmzd@outlook.com>
2022-11-27 22:55:53 -0500

    chore: write interfaces

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import "fmt"
+
+/**
+(AddTitle) -> (, AddContact) -> (,,AddExperience) -> ...
+*/
+
+
+type Resume struct {
+
+}
+
+type Experience struct {
+
+}
+
+type Project struct {
+
+}
+
+type Skills struct {
+
+}
+
+func AddContact() {
+
+}
+
+func AddProject() {
+
+}
+
+func AddExperience() {
+
+}
+
+func AddSkills() {
+
+}
+
+func main() {
+  fmt.Println("Hello, World!")
+}
