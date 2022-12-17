5e196c114e376b350551f71a37ba41c009a76ece
Urmzd Mukhammadnaim <urmzd@outlook.com>
2022-12-16 23:15:36 -0500

    refactor: add utility methods

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 )
 
 type Resume struct {
-	Contact     Contact
+	Contact     *Contact
 	Skills      []Detail
 	Experiences []Experience
 	Projects    []Project
@@ -19,42 +19,70 @@ type Resume struct {
 }
 
 type Education struct {
-	School   string
-	Degree   string
+	School   *string
+	Degree   *string
 	Suffixes []string
 	Details  []Detail
-	Location Location
-	Dates    DateRange
+	Location *Location
+	Dates    *DateRange
 }
 
 type Contact struct {
-	Name  string
-	Email string
-	Phone string
+	Name  *string
+	Email *string
+	Phone *string
 	Links []Link
 }
 
 type Link struct {
-	Text string
-	Link string
+	Text *string
+	Link *string
+}
+
+func NewLink(text string, link string, prefix string) *Link {
+	return &Link {
+		Text: prefix + text,
+		Link: link,
+	}
+}
+
+func (link *Link) toString() string {
+	return fmt.Sprintf(`\href{%s}{%s}`, *link.Text, *link.Link)
 }
 
 type Location struct {
-	City  string
-	State string
+	City  *string
+	State *string
+}
+
+func (loc *Location) toString() string {
+	return fmt.Sprintf("%s, %s", *loc.City, *loc.State)
 }
 
 type DateRange struct {
-	Start time.Time
-	End   time.Time
+	Start *time.Time
+	End   *time.Time
+}
+
+func (rng *DateRange) toString() string {
+	dateFmt := "Jan 2006"
+
+	var end string
+	if rng.End == nil {
+		end = "Present"
+	} else {
+		end = rng.End.Format(dateFmt)
+	}
+
+	return fmt.Sprintf("%s - %s", rng.Start.Format(dateFmt), end)
 }
 
 type Experience struct {
-	CompanyName string
-	Title       string
-	Achievement []string
-	Dates       DateRange
-	Location    Location
+	CompanyName  string
+	Title        string
+	Achievements []string
+	Dates        DateRange
+	Location     Location
 }
 
 type Project struct {
@@ -81,69 +109,106 @@ type DefaultResumeGenerator struct {
 	builder strings.Builder
 }
 
+func (generator *DefaultResumeGenerator) write(strs ...string) {
+	for _, str := range strs {
+		generator.builder.WriteString(strings.TrimSpace(str))
+		generator.builder.WriteString("\n")
+	}
+}
+
+func (generator *DefaultResumeGenerator) AddExperiences(experiences *[]Experience) {
+	beforeCode := `\section*{education}`
+
+	generator.write(beforeCode)
+
+	for _, experience := range *experiences {
+		dateRange := experience.Dates.toString()
+		generator.addProject(experience.Title, experience.CompanyName, dateRange)
+		generator.addSubProject(experience.Location.toString())
+		generator.addAchievements(experience.Achievements...)
+	}
+}
+
+func (generator *DefaultResumeGenerator) addAchievements(achievements ...string) {
+	beforeCode := `\achievements%`
+	stringTemplate := `[%s]`
+
+	generator.write(beforeCode)
+
+	for _, achievement := range achievements {
+		stringValue := fmt.Sprintf(stringTemplate, achievement)
+		generator.write(stringValue)
+	}
+}
+
+func (generator *DefaultResumeGenerator) addProject(left string, middle string, right string) {
+	stringTemplate := `\project{%s}{%s}{%s}`
+	stringValue := fmt.Sprintf(stringTemplate, left, middle, right)
+	generator.write(stringValue)
+}
+
+func (generator *DefaultResumeGenerator) addSubProject(label string) {
+	stringTemplate := `\subproject{%s}`
+	stringValue := fmt.Sprintf(stringTemplate, label)
+	generator.write(stringValue)
+}
+
+func (generator *DefaultResumeGenerator) addDescription(skills *[]Detail) {
+	beforeCode := `\begin{description}`
+	afterCode := `\end{description}`
+	stringTemplate := `\item[%s]{%s}`
+
+	generator.write(beforeCode)
+
+	for _, skill := range *skills {
+		skillTemplate := fmt.Sprintf(stringTemplate, skill.Category, skill.Value)
+		generator.write(skillTemplate)
+	}
+
+	generator.write(afterCode)
+}
+
+func (generator *DefaultResumeGenerator) AddSkills(skills *[]Detail) {
+	beforeCode := `\section*{skills}`
+
+	generator.write(beforeCode)
+	generator.addDescription(skills)
+}
+
 func (generator *DefaultResumeGenerator) StartResume(contact *Contact) {
 
 	// Ensure that all links have a text display.
 	FillMissingLinkParts(&contact.Links[0])
 	FillMissingLinkParts(&contact.Links[1])
 
-	beforeCode := `
-		\documentclass{resume}
-		\usepackage{geometry}
-		\usepackage{titlesec}
-		\usepackage[allcolors=blue]{hyperref}
-		\usepackage{helvet}
+	beforeCode := []string{
+		`\documentclass{resume}`,
+		`\usepackage{geometry}`,
+		`\usepackage{titlesec}`,
+		`\usepackage[]{hyperref}`,
+		`\usepackage{helvet}`,
+		`\geometry{a4paper,left=0.5in,right=0.5in,bottom=0.5in,top = 0.5in}`,
+		`\hypersetup {colorlinks=true,linkcolor=blue }`,
+		`\titleformat{\section}{\normalfont\Large\scshape\fontsize{12}{15}}{\thesection}{1em}{}[{\titlerule[0.8pt]}]`,
+		`\begin{document}`,
+		`\pagestyle{empty}`,
+	}
 
-		\geometry{
-				a4paper,
-				left=0.5in,
-				right=0.5in,
-				bottom=0.5in,
-				top = 0.5in,
-		}
-
-		\hypersetup {
-			colorlinks=true,
-			linkcolor=blue
-		}
-
-		\titleformat{\section}
-			{\normalfont\Large\scshape\fontsize{12}{15}}
-			{\thesection}{1em}
-			{}[{\titlerule[0.8pt]}]
-
-		\begin{document}
-		\pagestyle{empty}
-	`
-
-	fmt.Println(contact)
-
-	name := fmt.Sprintf(`\name{%s}`, contact.Name)
-	basics := fmt.Sprintf(
-		`\contact%%
-			{\href{mailto://%s}{%s}}
-			{\href{tel:%s}{%s}}
-			{\href{%s}{%s}}
-			{\href{%s}{%s}}
-		`,
-		contact.Email,
-		contact.Email,
-		contact.Phone,
-		contact.Phone,
-		contact.Links[0].Text,
-		contact.Links[0].Link,
-		contact.Links[1].Text,
-		contact.Links[1].Link,
+	name := fmt.Sprintf(`\name{%s}`, *contact.Name)
+	basics := fmt.Sprintf(`\contact{}{}{}{}`,
+		NewLink(*contact.Email, *contact.Email, "mailto://"),
+		NewLink(*contact.Phone, *contact.Phone, "tel:"),
+		contact.Links[0],
+		contact.Links[1]
 	)
 
-	generator.builder.WriteString(beforeCode)
-	generator.builder.WriteString(name)
-	generator.builder.WriteString(basics)
+	generator.write(beforeCode...)
+	generator.write(name, basics)
 }
 
 func FillMissingLinkParts(link *Link) {
-	if link.Text == "" {
-		parsedLinked , err := url.Parse(link.Link)
+	if link.Text == nil {
+		parsedLinked, err := url.Parse(*link.Link)
 
 		if err != nil {
 			panic(err)
@@ -151,9 +216,9 @@ func FillMissingLinkParts(link *Link) {
 
 		urlWithoutSchema := fmt.Sprintf("%s%s", parsedLinked.Hostname(), parsedLinked.Path)
 
-		link.Text = urlWithoutSchema
+		link.Text = &urlWithoutSchema
 	}
-} 
+}
 
 func main() {
 	filename := os.Args[1]
@@ -175,6 +240,8 @@ func main() {
 
 	resumeBuilder := &DefaultResumeGenerator{}
 
-	resumeBuilder.StartResume(&resume.Contact)
+	resumeBuilder.StartResume(resume.Contact)
+	resumeBuilder.AddSkills(&resume.Skills)
+	resumeBuilder.AddExperiences(&resume.Experiences)
 	fmt.Println(resumeBuilder.builder.String())
 }
